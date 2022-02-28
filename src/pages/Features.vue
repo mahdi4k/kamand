86aa27f144daee482ad9b7c94d804f458eeb02a8
mahdi2k <falahati.mahdi@yahoo.com>
2022-02-28 15:49:54 +0330

    feat: table dialog  add

--- src/pages/Features.vue
+++ src/pages/Features.vue
@@ -1,52 +1,333 @@
 <template>
-      <AccordionMenu
-        headerTitle="مشخصات"
-        accordionTitle="در این بخش می‌توانید مشخصات را وارد کنید"
-        @add-button-clicked="focusInput()"
-      >
-        <slot>
-          <q-form
-            @submit="onSubmit"
-            class="q-gutter-md"
-          >
-            <div class="q-gutter-sm items-end inputs-form row ">
-              <div class="col">
-                <span class="input-title">عنوان</span>
-                <q-input ref="focus" dense v-model="featuresInputData.title" outlined type="text" placeholder="عنوان"/>
-              </div>
+  <section>
+    <AccordionMenu
+      headerTitle="مشخصات"
+      accordionTitle="در این بخش می‌توانید مشخصات را وارد کنید"
+      @add-button-clicked="focusInput()"
+    >
+      <slot>
+        <q-form
+          @submit="onSubmit"
+          class="q-gutter-md"
+        >
+          <div class="q-gutter-sm items-end inputs-form row ">
+            <div class="col">
+              <span class="input-title">عنوان</span>
+              <q-input ref="focus" dense v-model="featuresInputData.title" outlined type="text" placeholder="عنوان"/>
+            </div>
 
-              <div class="col">
-                <span class="input-title">کد</span>
-                <q-input dense width="37" v-model="featuresInputData.code" outlined type="text"
-                          placeholder="کد"/>
-              </div>
+            <div class="col">
+              <span class="input-title">کد</span>
+              <q-input dense width="37" v-model="featuresInputData.code" outlined type="text"
+                       placeholder="کد"/>
+            </div>
 
-              <div class="col">
-                <span class="input-title">نوع</span>
-                <q-select use-input dense class="inputs-col" outlined v-model="featuresInputData.type"
-                          :options="options"
-                          :placeholder="!featuresInputData.type ? 'نوع' : ''"/>
-              </div>
+            <div class="col">
+              <span class="input-title">نوع</span>
+              <q-select use-input dense class="inputs-col" outlined v-model="featuresInputData.type"
+                        :options="options"
+                        :placeholder="!featuresInputData.type ? 'نوع' : ''"/>
+            </div>
 
-              <div class="col">
-                <span class="input-title">تگ</span>
-                <q-select use-input dense outlined v-model="featuresInputData.tag" :options="options"
-                          :placeholder="!featuresInputData.tag ? 'تگ' : ''"/>
-              </div>
+            <div class="col">
+              <span class="input-title">تگ</span>
+              <q-select use-input dense outlined v-model="featuresInputData.tag" :options="options"
+                        :placeholder="!featuresInputData.tag ? 'تگ' : ''"/>
+            </div>
 
-              <div class="col">
-                <span class="input-title">دسته‌بندی</span>
-                <q-select use-input dense outlined v-model="featuresInputData.category" :options="options"
-                          :placeholder="!featuresInputData.category ? 'دسته‌بندی' : ''"/>
-              </div>
+            <div class="col">
+              <span class="input-title">دسته‌بندی</span>
 
+
+              <q-select
+                use-input
+                outlined
+                v-model="featuresInputData.category"
+                :options="options"
+                :placeholder="!featuresInputData.category ? 'دسته‌بندی' : ''"
+                multiple
+                dense
+                map-options
+              >
+                <template v-slot:option="{ itemProps, itemEvents, opt, selected, toggleOption }">
+                  <q-item
+                    v-bind="itemProps"
+                    v-on="itemEvents"
+                    class="multiSelect"
+                  >
+
+                    <div class="multiSelectSection">
+                      <q-item-section >
+                        <q-checkbox :value="selected" @input="toggleOption(opt)"></q-checkbox>
+                      </q-item-section>
+                      <q-item-section>
+                        <q-item-label v-html="opt.label" ></q-item-label>
+                      </q-item-section>
+                    </div>
+
+                  </q-item>
+                </template>
+              </q-select>
+            </div>
+
+            <div>
+              <q-btn label="افزودن" type="submit" color="primary"/>
+            </div>
+          </div>
+        </q-form>
+      </slot>
+    </AccordionMenu>
+    <div class="br-line"></div>
+
+    <!--table-->
+    <p class="title q-mt-xl">مدیریت مشخصات</p>
+    <q-table
+      class=" border-radius k-grid "
+      :data="data"
+      :columns="columns"
+      separator="vertical"
+      hide-header
+      hide-bottom
+      row-key="name"
+    >
+      <template v-slot:top>
+        <q-form
+          @submit="onSubmit"
+          class="full-width"
+        >
+
+          <div class="q-gutter-sm q-pt-sm q-ml-none items-end inputs-form row ">
+            <div class="self-center first-col-table">
+              <span>ردیف</span>
+            </div>
+
+            <div class="col">
+              <q-input ref="focus" dense v-model="featuresInputData.title" outlined type="text" placeholder="عنوان"/>
+            </div>
+
+            <div class="col">
+              <q-input dense width="37" v-model="featuresInputData.code" outlined type="text"
+                       placeholder="کد"/>
+            </div>
+
+            <div class="col">
+              <q-select use-input dense class="inputs-col" outlined v-model="featuresInputData.type"
+                        :options="options"
+                        :placeholder="!featuresInputData.type ? 'نوع' : ''"/>
+            </div>
+
+            <div class="col">
+              <q-select use-input dense outlined v-model="featuresInputData.tag" :options="options"
+                        :placeholder="!featuresInputData.tag ? 'تگ' : ''"/>
+            </div>
+
+            <div class="col flex no-wrap">
+              <q-select
+                class="full-width"
+                multiple
+                use-input
+                dense outlined
+                v-model="featuresInputData.category"
+                :options="options"
+                :placeholder="!featuresInputData.category ? 'دسته‌بندی' : ''"/>
               <div>
-                <q-btn label="افزودن" type="submit" color="primary"/>
+                <q-btn class="filter-btn-dense" color="primary">
+                  <q-icon color="accent" size="22px">
+                    <img
+                      alt="Filter"
+                      src="~assets/icons/Filter.svg"
+                    >
+                  </q-icon>
+                </q-btn>
               </div>
             </div>
-          </q-form>
-        </slot>
-      </AccordionMenu>
+
+          </div>
+        </q-form>
+      </template>
+      <template v-slot:body="props">
+        <div class="row q-gutter-sm q-mt-none relative-position q-ml-none tableRows">
+          <div class="first-col-table">
+            <span key="name" :props="props" class="input-title">{{ props.row.name }}</span>
+          </div>
+          <div class="col detail">
+            <span key="name" :props="props" class="input-title">{{ props.row.carbs }}</span>
+          </div>
+          <div class="col detail">
+            <span key="name" :props="props" class="input-title">{{ props.row.protein }}</span>
+          </div>
+          <div class="col detail">
+            <span key="name" :props="props" class="input-title">{{ props.row.sodium }}</span>
+          </div>
+          <div class="col detail">
+            <span key="name" :props="props" class="input-title">{{ props.row.calcium }}</span>
+          </div>
+          <div class="col detail">
+            <span key="name" :props="props" class="input-title">{{ props.row.fat }}</span>
+          </div>
+          <div class="last-col-table">
+            <!--            parameter-->
+            <q-btn @click="parameterDialog = !parameterDialog" class="btn-dense" color="white">
+              <q-icon color="accent" size="22px">
+                <img
+                  alt="Parameters"
+                  src="~assets/icons/Parameters.svg"
+                >
+              </q-icon>
+              <q-tooltip>
+                پارامتر
+              </q-tooltip>
+            </q-btn>
+            <!--            edit-->
+            <q-btn @click="editDialog = !editDialog" class="btn-dense" color="white">
+              <q-icon color="accent" size="22px">
+                <img
+                  alt="Edit"
+                  src="~assets/icons/Edit.svg"
+                >
+              </q-icon>
+              <q-tooltip>
+                ویرایش
+              </q-tooltip>
+            </q-btn>
+            <!--            remove-->
+            <q-btn @click="removeDialog = !removeDialog" class="btn-dense" color="white">
+              <q-icon color="accent" size="22px">
+                <img
+                  alt="Remove"
+                  src="~assets/icons/Remove.svg"
+                >
+              </q-icon>
+              <q-tooltip>
+                حذف
+              </q-tooltip>
+            </q-btn>
+          </div>
+        </div>
+
+      </template>
+    </q-table>
+    <div class="row justify-between q-mt-md">
+      <q-pagination
+        v-model="pagination.page"
+        color="grey-8"
+        :max="pagesNumber"
+        boundary-numbers
+        class="btn-pagination"
+        size="sm"
+      />
+      <div class="flex  items-center">
+        <span>تعداد نمایش :</span>
+        <q-select use-input dense class="rowPerPage" outlined v-model="type"
+                  :options="optionPageSelect"
+        />
+      </div>
+    </div>
+
+    <!--    dialog section-->
+    <q-dialog v-model="removeDialog">
+      <q-card class="q-pa-lg border-radius-dialog" style="width: 400px">
+        <q-card-section class="d-grid text-right grid-column items-center q-pb-none">
+          <div class="text-h6">پیام</div>
+          <div>
+            <q-btn icon="close" flat round dense v-close-popup/>
+          </div>
+        </q-card-section>
+        <q-card-section class="row justify-center items-center">
+          <span class="q-ml-sm">آیا از حذف این سطر اطمینان دارید؟</span>
+        </q-card-section>
+
+        <q-card-actions align="center">
+          <q-btn class="cancel-btn" outline color="blue-grey-2" label="انصراف" v-close-popup></q-btn>
+          <q-btn class="q-px-md" color="green-9" text-color="white" label="بله"></q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="editDialog">
+      <q-card class="q-pa-lg border-radius-dialog" style="width: 700px; max-width: 80vw;">
+        <q-card-section class="d-grid text-right grid-column items-center q-pb-none">
+          <div class="text-h6">ویرایش</div>
+          <div>
+            <q-btn icon="close" flat round dense v-close-popup/>
+          </div>
+        </q-card-section>
+        <q-card-section class="row">
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-input ref="focus" dense v-model="edit.title" outlined type="text" placeholder="عنوان"/>
+          </div>
+
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-input dense width="37" v-model="edit.code" outlined type="text"
+                     placeholder="کد"/>
+          </div>
+
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-select use-input dense class="inputs-col" outlined v-model="edit.type"
+                      :options="options"
+                      :placeholder="!edit.type ? 'نوع' : ''"/>
+          </div>
+
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-select use-input dense outlined v-model="edit.tag" :options="options"
+                      :placeholder="!edit.tag ? 'تگ' : ''"/>
+          </div>
+
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-select class="full-width" use-input dense outlined v-model="edit.category"
+                      :options="options"
+                      :placeholder="!edit.category ? 'دسته‌بندی' : ''"/>
+          </div>
+        </q-card-section>
+
+        <q-card-actions class="q-pr-lg" align="right">
+          <q-btn class="q-px-xl" color="primary" label="ویرایش"></q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="parameterDialog">
+      <q-card class="q-pa-lg border-radius-dialog" style="width: 400px">
+        <q-card-section class="d-grid text-right grid-column items-center q-pb-none">
+          <div>افزودن پارامتر لیست</div>
+          <div>
+            <q-btn icon="close" flat round dense v-close-popup/>
+          </div>
+        </q-card-section>
+
+        <q-card-section class="row">
+          <div class="self-center first-col-table">
+            <span>پارامتر</span>
+          </div>
+          <q-input dense width="37" v-model="parameter" outlined type="text"
+                   placeholder="پارامتر"/>
+          <q-btn @click="addRow" :disable="!parameter.length" class="filter-btn-dense" color="primary">
+            <q-icon color="accent" size="22px">
+              <img
+                alt="Add"
+                src="~assets/icons/Add.svg"
+              >
+            </q-icon>
+          </q-btn>
+          <div v-for="(parameter,index) in parameterRow">
+            <q-input dense width="37" v-model="parameter.value" outlined type="text"
+                     placeholder="پارامتر"/>
+            <q-btn @click="removeRow(index)"   class="filter-btn-dense" color="primary">
+              <q-icon color="accent" size="22px">
+                <img
+                  alt="Add"
+                  src="~assets/icons/Add.svg"
+                >
+              </q-icon>
+            </q-btn>
+          </div>
+
+        </q-card-section>
+
+
+      </q-card>
+    </q-dialog>
+  </section>
 
 </template>
 
@@ -63,6 +344,89 @@ export default {
   components: {AccordionMenu},
   data() {
     return {
+      type: null,
+      options: [
+        {
+          label: 'عمومی',
+          value: 1
+        },
+        {
+          label: 'پر خطر',
+          value: 2
+        },
+        {
+          label: 'الکترود',
+          value: 3
+        },
+
+      ],
+      optionPageSelect: [1, 5, 10, 15],
+      removeDialog: false,
+      parameter: [],
+      parameterRow: [],
+      editDialog: false,
+      parameterDialog: false,
+      pagination: {
+        sortBy: 'desc',
+        descending: false,
+        page: 3,
+        rowsPerPage: 3
+        // rowsNumber: xx if getting data from a server
+      },
+      pagesNumber: 2,
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: row => row.name,
+          sortable: true
+        },
+
+        {name: 'carbs', label: 'Carbs (g)', field: 'carbs'},
+        {name: 'protein', label: 'Protein (g)', field: 'protein'},
+        {name: 'sodium', label: 'Sodium (mg)', field: 'sodium'},
+        {
+          name: 'calcium',
+          label: 'Calcium (%)',
+          field: 'calcium',
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+        },
+        {
+          name: 'iron',
+          label: 'Iron (%)',
+          field: 'iron',
+          sortable: true,
+          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+        }
+      ],
+      data: [
+        {
+          name: 1,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+        },
+        {
+          name: 2,
+          fat: 'آج',
+          carbs: 224,
+          protein: 'لیست',
+          sodium: 'DRI-Co',
+          calcium: 'درخت تجهیز',
+        }, {
+          name: 2,
+          fat: 'آج',
+          carbs: 224,
+          protein: 'لیست',
+          sodium: 'DRI-Co',
+          calcium: 'درخت تجهیز',
+        },
+      ],
       featuresInputData: {
         title: null,
         code: null,
@@ -70,9 +434,13 @@ export default {
         tag: null,
         category: null
       },
-      options: [
-        'a', 'b', 'c'
-      ]
+      edit: {
+        title: null,
+        code: null,
+        type: null,
+        tag: null,
+        category: null
+      },
     }
   },
   methods: {
@@ -86,7 +454,17 @@ export default {
         this.$refs.focus.focus()
       }, 100)
 
+    },
+    addRow(){
+      this.parameterRow.push({value :null})
+    },
+    removeRow(index){
+      console.log(this.parameterRow.splice(index,1))
     }
   }
 }
 </script>
+<style lang="scss">
+@import "src/css/table";
+
+</style>
