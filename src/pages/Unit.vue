0febc8331379d8e277d9c5e4bc3071ae9a334732
mahdi2k <falahati.mahdi@yahoo.com>
2022-02-28 15:49:54 +0330

    feat: table dialog  add

--- src/pages/Unit.vue
+++ src/pages/Unit.vue
@@ -25,8 +25,9 @@
     </AccordionMenu>
     <div class="br-line"></div>
 
+<!--table-->
     <q-table
-      class="q-mt-xl border-radius "
+      class="q-mt-xl border-radius k-grid "
       :data="data"
       :columns="columns"
       separator="vertical"
@@ -36,12 +37,11 @@
     >
       <template v-slot:top>
         <q-form
-
           @submit="onSubmit"
           class="full-width"
         >
 
-          <div class="q-gutter-sm q-ml-none items-end inputs-form row ">
+          <div class="q-gutter-sm q-pt-sm q-ml-none items-end inputs-form row ">
             <div class="self-center first-col-table">
               <span>ردیف</span>
             </div>
@@ -66,46 +66,83 @@
                         :placeholder="!featuresInputData.tag ? 'تگ' : ''"/>
             </div>
 
-            <div class="col">
-              <q-select use-input dense outlined v-model="featuresInputData.category" :options="options"
+            <div class="col flex no-wrap">
+              <q-select class="full-width" use-input dense outlined v-model="featuresInputData.category"
+                        :options="options"
                         :placeholder="!featuresInputData.category ? 'دسته‌بندی' : ''"/>
+              <div>
+                <q-btn class="filter-btn-dense" color="primary">
+                  <q-icon color="accent" size="22px">
+                    <img
+                      alt="Filter"
+                      src="~assets/icons/Filter.svg"
+                    >
+                  </q-icon>
+                </q-btn>
+              </div>
             </div>
 
-            <div>
-              <q-btn color="primary">
-                <q-icon color="accent" size="22px">
-                  <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd"
-                          d="M22 0H-1.90735e-06L7.93837 13.389V22H14.0612V13.389L22 0ZM18.3683 2.09524L12.0202 12.8015V19.9048H9.97931V12.8015L3.63155 2.09524H18.3683Z"
-                          fill="white"/>
-                  </svg>
-                </q-icon>
-              </q-btn>
-            </div>
           </div>
         </q-form>
       </template>
       <template v-slot:body="props">
-        <div class="row q-gutter-sm q-ml-none tableRows">
+        <div class="row q-gutter-sm q-mt-none relative-position q-ml-none tableRows">
           <div class="first-col-table">
             <span key="name" :props="props" class="input-title">{{ props.row.name }}</span>
           </div>
-          <div class="col">
+          <div class="col detail">
             <span key="name" :props="props" class="input-title">{{ props.row.carbs }}</span>
           </div>
-          <div class="col">
+          <div class="col detail">
             <span key="name" :props="props" class="input-title">{{ props.row.protein }}</span>
           </div>
-          <div class="col">
+          <div class="col detail">
             <span key="name" :props="props" class="input-title">{{ props.row.sodium }}</span>
           </div>
-          <div class="col">
+          <div class="col detail">
             <span key="name" :props="props" class="input-title">{{ props.row.calcium }}</span>
           </div>
-          <div class="col">
+          <div class="col detail">
             <span key="name" :props="props" class="input-title">{{ props.row.fat }}</span>
           </div>
-          <div class="last-col-table"></div>
+          <div class="last-col-table">
+            <!--            parameter-->
+            <q-btn @click="parameterDialog = !parameterDialog" class="btn-dense" color="white">
+              <q-icon color="accent" size="22px">
+                <img
+                  alt="Parameters"
+                  src="~assets/icons/Parameters.svg"
+                >
+              </q-icon>
+              <q-tooltip>
+                پارامتر
+              </q-tooltip>
+            </q-btn>
+            <!--            edit-->
+            <q-btn @click="editDialog = !editDialog" class="btn-dense" color="white">
+              <q-icon color="accent" size="22px">
+                <img
+                  alt="Edit"
+                  src="~assets/icons/Edit.svg"
+                >
+              </q-icon>
+              <q-tooltip>
+                ویرایش
+              </q-tooltip>
+            </q-btn>
+            <!--            remove-->
+            <q-btn @click="removeDialog = !removeDialog" class="btn-dense" color="white">
+              <q-icon color="accent" size="22px">
+                <img
+                  alt="Remove"
+                  src="~assets/icons/Remove.svg"
+                >
+              </q-icon>
+              <q-tooltip>
+                حذف
+              </q-tooltip>
+            </q-btn>
+          </div>
         </div>
 
       </template>
@@ -126,6 +163,111 @@
         />
       </div>
     </div>
+
+<!--    dialog section-->
+    <q-dialog v-model="removeDialog">
+      <q-card class="q-pa-lg border-radius-dialog" style="width: 400px">
+        <q-card-section class="d-grid text-right grid-column items-center q-pb-none">
+          <div class="text-h6">پیام</div>
+          <div>
+            <q-btn icon="close" flat round dense v-close-popup/>
+          </div>
+        </q-card-section>
+        <q-card-section class="row justify-center items-center">
+          <span class="q-ml-sm">آیا از حذف این سطر اطمینان دارید؟</span>
+        </q-card-section>
+
+        <q-card-actions align="center">
+          <q-btn class="cancel-btn" outline color="blue-grey-2" label="انصراف" v-close-popup></q-btn>
+          <q-btn class="q-px-md" color="green-9" text-color="white" label="بله"></q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="editDialog">
+      <q-card class="q-pa-lg border-radius-dialog" style="width: 700px; max-width: 80vw;">
+        <q-card-section class="d-grid text-right grid-column items-center q-pb-none">
+          <div class="text-h6">ویرایش</div>
+          <div>
+            <q-btn icon="close" flat round dense v-close-popup/>
+          </div>
+        </q-card-section>
+        <q-card-section class="row">
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-input ref="focus" dense v-model="edit.title" outlined type="text" placeholder="عنوان"/>
+          </div>
+
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-input dense width="37" v-model="edit.code" outlined type="text"
+                     placeholder="کد"/>
+          </div>
+
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-select use-input dense class="inputs-col" outlined v-model="edit.type"
+                      :options="options"
+                      :placeholder="!edit.type ? 'نوع' : ''"/>
+          </div>
+
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-select use-input dense outlined v-model="edit.tag" :options="options"
+                      :placeholder="!edit.tag ? 'تگ' : ''"/>
+          </div>
+
+          <div class="col-4 q-pr-sm q-mb-md">
+            <q-select class="full-width" use-input dense outlined v-model="edit.category"
+                      :options="options"
+                      :placeholder="!edit.category ? 'دسته‌بندی' : ''"/>
+          </div>
+        </q-card-section>
+
+        <q-card-actions class="q-pr-lg" align="right">
+          <q-btn class="q-px-xl" color="primary" label="ویرایش"></q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="parameterDialog">
+      <q-card class="q-pa-lg border-radius-dialog" style="width: 400px">
+        <q-card-section class="d-grid text-right grid-column items-center q-pb-none">
+          <div>افزودن پارامتر لیست</div>
+          <div>
+            <q-btn icon="close" flat round dense v-close-popup/>
+          </div>
+        </q-card-section>
+
+        <q-card-section class="row">
+          <div class="self-center first-col-table">
+            <span>پارامتر</span>
+          </div>
+          <q-input dense width="37" v-model="parameter" outlined type="text"
+                   placeholder="پارامتر"/>
+          <q-btn @click="addRow" :disable="!parameter.length" class="filter-btn-dense" color="primary">
+            <q-icon color="accent" size="22px">
+              <img
+                alt="Add"
+                src="~assets/icons/Add.svg"
+              >
+            </q-icon>
+          </q-btn>
+          <div v-for="(parameter,index) in parameterRow">
+            <q-input dense width="37" v-model="parameter.value" outlined type="text"
+                     placeholder="پارامتر"/>
+            <q-btn @click="removeRow(index)"   class="filter-btn-dense" color="primary">
+              <q-icon color="accent" size="22px">
+                <img
+                  alt="Add"
+                  src="~assets/icons/Add.svg"
+                >
+              </q-icon>
+            </q-btn>
+          </div>
+
+        </q-card-section>
+
+
+      </q-card>
+    </q-dialog>
+
   </section>
 </template>
 
@@ -144,7 +286,12 @@ export default {
     return {
       type: null,
       options: ['a', 'b', 'c'],
-      optionPageSelect:[1,5,10,15],
+      optionPageSelect: [1, 5, 10, 15],
+      removeDialog: false,
+      parameter: [],
+      parameterRow: [],
+      editDialog: false,
+      parameterDialog: false,
       pagination: {
         sortBy: 'desc',
         descending: false,
@@ -197,7 +344,7 @@ export default {
           protein: 'لیست',
           sodium: 'DRI-Co',
           calcium: 'درخت تجهیز',
-        },{
+        }, {
           name: 2,
           fat: 'آج',
           carbs: 224,
@@ -213,47 +360,31 @@ export default {
         tag: null,
         category: null
       },
+      edit: {
+        title: null,
+        code: null,
+        type: null,
+        tag: null,
+        category: null
+      },
     }
   },
   methods: {
     onSubmit() {
 
+    },
+    addRow(){
+      this.parameterRow.push({value :null})
+    },
+    removeRow(index){
+      console.log(this.parameterRow.splice(index,1))
     }
-  }
+  },
+
 }
 </script>
 
 <style lang="scss">
-.first-col-table {
-  margin-right: 0;
-  width: 4%;
-  text-align: center;
-  height: 100%;
-  place-self: center;
-}
-.btn-pagination{
-  .q-btn__content{
-    background: white;
-    padding: 8px 14px 8px 14px;
-    border: 1px solid #CCD9E5;
-    border-radius: 8px;
-  }
-  .q-focus-helper {
-    visibility: hidden;
-  }
-}
-.last-col-table{
-  width: 3%;
-}
-.rowPerPage{
-  width: 100px;
-  background-color: white;
-}
-.tableRows {
-  padding: 12px 16px;
-  border-bottom: 1px solid #CCD9E5;
-}
-.tableRows:last-child{
-  border-bottom: unset;
-}
+@import "src/css/table";
+
 </style>
